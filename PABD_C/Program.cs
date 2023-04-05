c61ca24d5af90077b96c0278b73a85cbcbe33356
Nanang Puji <nanangpuji762@gmail.com>
2023-04-05 17:00:58 +0700

    Add project files.

---
+++ PABD_C/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Insert_and_Get_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password :");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database Tujuan :");
+                    string db = Console.ReadLine();
+                    Console.WriteLine("\nKetik K untuk Terhubung ke Database :");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source = INAYAHMUFIDAH\\PIYOMARU842; " +
+                                    "initial catalog = {0};" +
+                                    "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat Seluruh Data");
+                                        Console.WriteLine("2. Tambah Data");
+                                        Console.WriteLine("3. Keluar");
+                                        Console.Write("\nEnter your choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                        switch (ch)
+                                        {
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("DATA MAHASISWA\n");
+                                                    Console.WriteLine();
+                                                    pr.baca(conn);
+                                                }
+                                                break;
+                                            case '2':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("INPUT DATA MAHASISWA\n");
+                                                    Console.WriteLine("Masukkan NIM :");
+                                                    string NIM = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Mahasiswa :");
+                                                    string NmaMhs = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Alamat Mahasiswa :");
+                                                    string Almt = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Jenis Kelamin (L/P) :");
+                                                    string jk = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No Telepon :");
+                                                    string notlpn = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        pr.insert(NIM, NmaMhs, Almt, jk, notlpn, conn);
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            case '3':
+                                                conn.Close();
+                                                return;
+                                            default:
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("\nInvalid option");
+                                                }
+                                                break;
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        Console.Clear();
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("Tidak Dapat Mengakses Database Menggunakan User Tersebut\n");
+                                        Console.WriteLine();
+                                    }
+                                    r.Clsoe();
+                                }
+                                public void insert(string NIM, string NmaMhs, string Almt, string jk, string notlpn, 
+                                      Sqlcon
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
